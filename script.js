dd0000706d2090320ed9622b27d9ec273bb2c92b
Danilo Valério <140651234+danilopvalerio@users.noreply.github.com>
2025-08-19 17:04:07 -0300

    Feat: Segunda alteração para o commit extra

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function verificarNumero(num) {
   if (num > 0) {
-    console.log("O número é positivo.");
+    console.log("O número é maior que zero.");
   } else if (num < 0) {
-    console.log("O número é negativo.");
+    console.log("O número é menor que zero.");
   } else {
     console.log("O número é zero.");
   }
